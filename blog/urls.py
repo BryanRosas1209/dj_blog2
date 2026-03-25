6a6f2c7ef1cc0329b0139719efad924cc4587724
BryanRosas1209 <bryann.rosaaas@gmail.com>
2026-03-24 17:31:54 -0700

    CheProfeChingonLoAmo

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin 
 from django.urls import path 
-from views import home, detail
+from .views import home, detail
 
 urlpatterns = [
     path('', home, name = 'home'),
